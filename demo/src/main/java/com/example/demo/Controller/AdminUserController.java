7fb6949d3611fe67860b7da6ff8a1b637ec943a7
youngyoung0 <77282011+youngyoung0@users.noreply.github.com>
2021-10-31 22:23:59 +0900

    Filtering 전체사용자 조회 코드 업데이트

--- demo/src/main/java/com/example/demo/Controller/AdminUserController.java
+++ demo/src/main/java/com/example/demo/Controller/AdminUserController.java
@@ -23,8 +23,16 @@ public class AdminUserController {
 	UserDaoService service;
 	
 	@GetMapping("/users")
-	public List<User> retrieveAllUsers(){
-		return service.findAll();
+	public MappingJacksonValue retrieveAllUsers(){
+		List<User> users = service.findAll();
+		SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter
+				.filterOutAllExcept("id","name","joinDate","password");
+		
+		FilterProvider filters = new SimpleFilterProvider().addFilter("UserInfo",filter);
+		
+		MappingJacksonValue mapping = new MappingJacksonValue(users);
+		mapping.setFilters(filters);
+		return mapping;
 	}
 	
 	@GetMapping("/users/{id}")
