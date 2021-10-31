900ba141d76fad003e1b0da343487b1a4aa79e57
youngyoung0 <77282011+youngyoung0@users.noreply.github.com>
2021-10-31 23:34:53 +0900

    swagger 추가

--- demo/src/main/java/com/example/demo/Entity/User.java
+++ demo/src/main/java/com/example/demo/Entity/User.java
@@ -7,10 +7,8 @@ import java.util.Date;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
-import com.fasterxml.jackson.annotation.JsonFilter;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,14 +19,19 @@ import lombok.NoArgsConstructor;
 //@JsonIgnoreProperties(value= {"password","ssn"})
 @NoArgsConstructor
 //@JsonFilter("userInfo")
+@ApiModel(description = "사용자 상세 정보를 위한 객체 도메인 객체 ")
 public class User {
 
 	private Integer user_id;
 	@Size(min =2)
+	@ApiModelProperty(notes="사용자 이름을 입력해 주세요.")
 	private String name;
 	@Past // 미래 데이터 사용 불가 과거시간만 사용가능
+	@ApiModelProperty(notes="사용자 등록일을 입력해 주세요.")
 	private Date joinDate;
 	
+	@ApiModelProperty(notes="사용자 비밀번호를 입력해 주세요.")
 	private String password;
+	@ApiModelProperty(notes="사용자 주민번호를 입력해 주세요.")
 	private String ssn;
 }
